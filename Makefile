f2a0efcfbe6b21979cfcd8eef3801cbe3847074c
Mario Juric <mjuric@ias.edu>
2010-01-23 17:59:56 -0500

    Removed the separate swarm_test_hermite_gpu/cpu executables

--- Makefile
+++ Makefile
@@ -11,34 +11,27 @@ LDFLAGS=-L /usr/local/cuda/lib
 
 LIBPEYTON=src/astro/BinaryStream.o
 OBJECTS= src/swarmlib.o src/swarm.cu_o $(LIBPEYTON)
-EXEOBJECTS = src/swarm.o src/swarmdump.o src/swarm_test_hermite_gpu.o
+EXEOBJECTS = src/swarm.o src/swarmdump.o
 ALLOBJECTS = $(OBJECTS) $(EXEOBJECTS)
 
-all: bin/swarm bin/swarmdump bin/swarm_test_hermite_gpu
+all: bin/swarm bin/swarmdump
 
 #### Integrators
 include integrators/*/Makefile.mk
 ####
 
-bench: all
-	(cd run && ../bin/easyGen.py && ../bin/swarm)
-
-bin/swarm: $(OBJECTS) src/swarm.o 
+bin/swarm: $(OBJECTS) src/swarm.o
 	$(CXX) -rdynamic -o $@ $(LDFLAGS) -lcuda -lcudart $(OBJECTS) src/swarm.o
 
 src/swarm.o: src/swarm.h
 
 test: all
-	(cd run && (test -f data.0 || ../bin/easyGen.py) && ../bin/swarm_test_hermite_gpu && ../bin/swarmdump)
+	(cd run && (test -f data.0 || ../bin/easyGen.py) && ../bin/swarm && ../bin/swarmdump)
 
 
 bin/swarmdump: $(OBJECTS) src/swarmdump.o
 	$(CXX) -rdynamic -o $@ $(LDFLAGS) -lcuda -lcudart $(OBJECTS)  src/swarmdump.o
 bin/swarmdump.o: src/swarm.h src/swarmio.h
-
-bin/swarm_test_hermite_gpu: $(OBJECTS) src/swarm_test_hermite_gpu.o 
-	$(CXX) -rdynamic -o $@ $(LDFLAGS) -lcuda -lcudart $(OBJECTS)  src/swarm_test_hermite_gpu.o
-bin/swarm_test_hermite_gpu.o: src/swarm.h src/swarmio.h
 
 
 src/swarmlib.o: src/swarm.h src/swarmio.h
@@ -47,7 +40,7 @@ src/swarm.cu: src/swarm.h $(CUDA_DEPS)
 	./bin/combine_cu_files.sh src/swarmlib.cu integrators/*/*.cu >> $@
 
 clean:
-	rm -f $(ALLOBJECTS) *.linkinfo src/astro/*.o src/swarm.cu bin/swarmdump bin/swarm integrators/*/*.o bin/swarm_test_hermite_gpu
+	rm -f $(ALLOBJECTS) *.linkinfo src/astro/*.o src/swarm.cu bin/swarmdump bin/swarm integrators/*/*.o
 
 tidy: clean
 	rm -f *~ src/*~ src/astro/*~ integrators/*/*~ DEADJOE
--- src/swarm.cpp
+++ src/swarm.cpp
@@ -1,132 +1,84 @@
-#include <cuda_runtime_api.h>
 #include "swarm.h"
+#include "swarmio.h"
 #include <memory>
 #include <cmath>
 #include <iostream>
 #include <valarray>
+#include <sstream>
+#include <fstream>
 #include <vector>
 
-// compute the energies of masless bodies in an external potential
-// NOTE: This is a temporary function to support the mock "Euler" integrator
-void one_body_energy(std::valarray<double> &E, const cpu_ensemble &ens)
-{
-	E.resize(ens.nbod()*ens.nsys());
-
-	for(int sys = 0; sys != ens.nsys(); sys++)
-	{
-		for(int bod = 0; bod != ens.nbod(); bod++)
-		{
-			float m; double x[3], v[3];
-			ens.get_body(sys, bod, m, x[0], x[1], x[2], v[0], v[1], v[2]);
-
-			// assuming one-body problem
-			E[sys*ens.nbod() + bod] = (v[0]*v[0] + v[1]*v[1] + v[2]*v[2]) / 2. - 1. / sqrt(x[0]*x[0] + x[1]*x[1] + x[2]*x[2]);
-		}
-	}
-}
-
-// aux class to sort indices by energy error (biggest first)
-struct energy_sorter
-{
-	const std::valarray<double> &dE;
-	int nbod;
-
-	energy_sorter(const std::valarray<double> &dE_, int nbod_) : dE(dE_), nbod(nbod_) {}
-	bool operator()(const std::pair<int,int>  &a, const std::pair<int,int>  &b) const
-	{
-		double dEa = fabs(dE[a.first*nbod + a.second]);
-		double dEb = fabs(dE[b.first*nbod + b.second]);
-		if(isnan(dEa)) { dEa = 0.; }
-		if(isnan(dEb)) { dEb = 0.; }
-		return dEa > dEb;
-	}
-};
-
-// just a simple dump to stdout of the first N planets given by indices in idx
-void write_output_aux(const std::vector<std::pair<int,int> > &idx, const int N, const cpu_ensemble &ens, const std::valarray<double> &E, const std::valarray<double> &dEoverE)
-{
-	for(int i = 0; i != N; i++)
-	{
-		int sys = idx[i].first;
-		int bod = idx[i].second;
-
-		float m; double x[3], v[3];
-		ens.get_body(sys, bod, m, x[0], x[1], x[2], v[0], v[1], v[2]);
-
-		printf("%5d %5d  T=%f  m=%f  pos=(% 9.5f % 9.5f % 9.5f)  vel=(% 9.5f % 9.5f % 9.5f)  E=% 9.5f dE/E=% 9.5f\n", sys, bod, ens.time(sys), m, x[0], x[1], x[2], v[0], v[1], v[2], E[sys*ens.nbod() + bod], dEoverE[sys*ens.nbod() + bod]);
-	}
-}
-
-void write_output(cpu_ensemble &ens, std::valarray<double> &E)
-{
-	std::valarray<double> Enew;
-	one_body_energy(Enew, ens);
-	std::valarray<double> dEoverE(Enew.size());
-	bool has_dE = E.size() == Enew.size();
-	if(has_dE)
-	{
-		dEoverE = Enew / E - 1.;
-	}
-	else
-	{
-		E.resize(Enew.size());
-		dEoverE = 0.;
-	}
-	E = Enew;
-
-	std::vector<std::pair<int,int> > idx; idx.reserve(ens.nsys()*ens.nbod());
-	for(int i = 0; i != ens.nsys(); i++) for(int j = 0; j != ens.nbod(); j++) idx.push_back(std::make_pair(i, j));
-
-	// print out the first few systems
-	write_output_aux(idx, std::min(2, ens.nsys())*ens.nbod(), ens, E, dEoverE);
-
-	if(has_dE)
-	{
-		// find the planets with worst energy conservation and print them out
-		std::sort(idx.begin(), idx.end(), energy_sorter(dEoverE, ens.nbod()));
-		std::cerr << "Planets with worst energy conservation (excluding those w. infinite energy):\n";
-		write_output_aux(idx, std::min(2, ens.nsys())*ens.nbod(), ens, E, dEoverE);
-	}
-}
+#define SWATCH_STOP(s)  { cudaThreadSynchronize(); (s).stop(); }
+#define SWATCH_START(s) { (s).start(); }
 
 int main()
 {
 	// load the ensemble
 	cpu_ensemble ens;
 	load_ensemble("data", ens);
-	std::valarray<double> E;
-	std::cerr << "Initial conditions of a first few systems:\n";
-	write_output(ens, E);
+	unsigned int nprint = std::min(2, ens.nsys());
 
-	// set up the integrator and integrator config (TODO: load from config file)
+	ens_writer out("output.bin");
+	out << ens;
+
+	// performance stopwatches
+	stopwatch swatch_kernel, swatch_mem, swatch_temps, swatch_all;
+
+	// set up the integrator
+	SWATCH_START(swatch_all);
 	config cfg;
-	cfg["integrator"] = "gpu_euler";
-	//cfg["integrator"] = "gpu_mvs";
-	//cfg["integrator"] = "gpu_does_not_exist";
-	cfg["h"] = "0.001";
-	std::auto_ptr<integrator> integ( integrator::create(cfg) );
+	load_config(cfg, "integrator.cfg");
+	std::auto_ptr<integrator> integ(integrator::create(cfg));
+	std::string runon = cfg.count("runon") ? cfg["runon"] : "gpu";
+	bool ongpu;
+	     if(runon == "gpu") { ongpu = true; }
+	else if(runon == "cpu") { ongpu = false; }
+	else { ERROR("The 'runon' configuration file parameter must be one of 'gpu' or 'cpu'"); }
+	std::cerr << "Integrator: " << cfg["integrator"] << ", executing on the " << (ongpu ? "GPU" : "CPU") << "\n";
+	// duration of integration
+	double dT;
+	get_config(dT, cfg, "dT");
 
 	// perform the integration
-	stopwatch swatch_kernel, swatch_all;
-	const double dT = 1;	// duration of integration (TODO: read from config file)
-	
-	swatch_all.start();
-	gpu_ensemble gpu_ens(ens);				// upload to GPU
-	swatch_kernel.start();
-	integ->integrate(gpu_ens, dT);				// integrate
-	cudaThreadSynchronize(); swatch_kernel.stop();
-	ens.copy_from(gpu_ens);					// download to host
-	cudaThreadSynchronize(); swatch_all.stop();
+	if(ongpu)
+	{
+		SWATCH_START(swatch_mem);
+		gpu_ensemble gpu_ens(ens);				// upload to GPU
+		SWATCH_STOP(swatch_mem);
 
-	// store output
-	std::cerr << "Final state of a first few systems:\n";
-	write_output(ens, E);
+		SWATCH_START(swatch_temps);
+		integ->integrate(gpu_ens, 0.);				// initialize internal data structures
+		SWATCH_STOP(swatch_temps);
+
+		SWATCH_START(swatch_kernel);
+		integ->integrate(gpu_ens, dT);				// integrate
+		SWATCH_STOP(swatch_kernel);
+
+		SWATCH_START(swatch_mem);
+		ens.copy_from(gpu_ens);					// download to host
+		SWATCH_STOP(swatch_mem);
+	}
+	else
+	{
+		SWATCH_START(swatch_temps);
+		integ->integrate(ens, 0.);				// initialize internal data structures
+		SWATCH_STOP(swatch_temps);
+
+		SWATCH_START(swatch_kernel);
+		integ->integrate(ens, dT);				// integrate
+		SWATCH_STOP(swatch_kernel);
+	}
+	SWATCH_STOP(swatch_all);
+
+	out << ens;
 
 	// print out timings
 	double us_per_sys_all = (swatch_all.getTime() / ens.nsys()) * 1000000;
 	double us_per_sys_kernel = (swatch_kernel.getTime() / ens.nsys()) * 1000000;
-	std::cerr << "Time per system (integration): " << us_per_sys_kernel << " us.\n";
-	std::cerr << "Time per system (mem+integr.): " << us_per_sys_all << " us.\n";
+	std::cerr << "Time per system (integration)   : " << us_per_sys_kernel << " us.\n";
+	std::cerr << "Time per system (setup+integr.) : " << us_per_sys_all << " us.\n";
+	std::cerr << "GPU/CPU memcpy time             : " << swatch_mem.getTime()*1000 << " ms.\n";
+	std::cerr << "Internal state initialization   : " << swatch_temps.getTime()*1000 << " ms.\n";
 
 	// both the integrator & the ensembles are automatically deallocated on exit
 	// so there's nothing special we have to do here.
