5ae05b289d87714a0d870615995a6d9614db0474
Mario Juric <mjuric@corrin.cfa.harvard.edu>
2010-01-29 00:25:31 -0500

    Debugging eventlog problems when running on a real GPU

--- src/swarmlib.cu
+++ src/swarmlib.cu
@@ -85,7 +85,7 @@ __device__ void output_if_needed(L &log, ensemble &ens, double T, int sys)
 	if(T >= ens.time_output(sys, 0) && sys < 10)
 	{
 		// for debugging
-		log.printf("Stored a system snapshot: sys=%d, T=%f (Tnext=%f).", sys, T, ens.time_output(sys, 0));
+		log.printf("Stored a snapshot: sys=%d, T=%f (Tnext=%f).", sys, T, ens.time_output(sys, 0));
 
 		int evtref = glog.log_event(EVT_SNAPSHOT, sys, T);
 		log.log_system(ens, sys, T, evtref);
@@ -129,6 +129,8 @@ __device__ void generic_integrate_system(retval_t *retval, ensemble &ens, int sy
 template<typename stopper_t, typename propagator_t>
 __global__ void gpu_integ_driver(retval_t *retval, int max_steps, propagator_t H, stopper_t stop)
 {
+//	glog.printf("This is a test printout.");
+//	glog.printf("And another %d one.", max_steps);
 	// find the system we're to work on
 	ensemble &ens = gpu_integ_ens;
 	int sys = threadId();
