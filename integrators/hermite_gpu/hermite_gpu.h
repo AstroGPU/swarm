870721e92d1948b659c85f7f5e8b733a79241647
Young Yeo <yiyeo@tesla4.ufhpc>
2010-04-13 22:44:28 -0400

    struct gpu_hermite_integrator_data is deleted from hermite_gpu.h

--- integrators/hermite_gpu/hermite_gpu.h
+++ integrators/hermite_gpu/hermite_gpu.h
@@ -6,32 +6,10 @@
 namespace swarm {
 namespace hermite_gpu {
 
-template<typename real_hi, typename real_lo>
-struct gpu_hermite_integrator_data
-{
-  // Typedef's (not clear if Jianwei is using these throughout
-  typedef real_hi real;
-  typedef real_hi real_time;
-  typedef float   real_mass;
-  typedef real_hi real_pos;
-  typedef real_hi real_vel;
-  typedef real_lo real_acc;
-  typedef real_lo real_jerk;
-
-  // Integration state
-  real_pos        *m_xyz_old;
-  real_vel        *m_vxyz_old;
-  real_acc        *m_acc, *m_acc_old;
-  real_jerk       *m_jerk, *m_jerk_old;
-
-  // Other parameters the integrator will need
-  float h;
-};
-
 
 //class gpu_hermite_integrator : public integrator
 template< typename real_hi, typename real_lo>
-class gpu_hermite_integrator : public integrator, public gpu_hermite_integrator_data<real_hi,real_lo>
+class gpu_hermite_integrator : public integrator
 {
 protected:
 	float h;
