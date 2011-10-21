5c2d86b195181b4300efbff476db9eb8588e3810
Saleh Dindar <sale@cise.ufl.edu>
2011-10-21 02:07:29 -0400

    Smalltests for CPU and Log testing. Still has to figure out how to test the log

--- test.cmake
+++ test.cmake
@@ -4,6 +4,8 @@ ENABLE_TESTING()
 
 ADD_TEST(NAME basic COMMAND swarm integrate --defaults )
 
+ADD_TEST(NAME CPU COMMAND swarm test -c ${SAMPLES}/hermite_cpu.cfg -I ${SAMPLES}/smalltest.in.txt -O ${SAMPLES}/smalltest.out.txt )
+
 MACRO(TEST_INTEGRATOR title cfgname)
 	ADD_TEST(NAME Verify_${title}
 		COMMAND swarm test -c ${SAMPLES}/${cfgname} -I ${SAMPLES}/test.in.txt -O ${SAMPLES}/test.out.txt -v 100)
@@ -12,7 +14,6 @@ ENDMACRO(TEST_INTEGRATOR title cfgname)
 
 TEST_INTEGRATOR(Hermite_GPU hermite.cfg)
 TEST_INTEGRATOR(Hermite_adap_GPU hermite_adap.cfg)
-TEST_INTEGRATOR(Hermite_CPU hermite_cpu.cfg)
 TEST_INTEGRATOR(Runge_Kutta_Fixed_Time_Step rkckf.cfg)
 TEST_INTEGRATOR(Runge_Kutta_Adaptive_Time_Step rkcka.cfg)
 TEST_INTEGRATOR(Euler euler.cfg)
