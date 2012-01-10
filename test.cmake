af0bd0e0c315c0bd679d8de47d24d9fc750809fc
Saleh Dindar <salehqt@gmail.com>
2012-01-09 20:36:18 -0500

    Test Integrator X Nbod.

--- test.cmake
+++ test.cmake
@@ -1,20 +1,37 @@
 # Use ${SAMPLES} if you need to reference any sample input or configuration file
 
 ENABLE_TESTING()
+MACRO(TEST_INTEGRATOR title nbod)
+	ADD_TEST(NAME Verify_${title}_${nbod}_Bodies
+		COMMAND swarm test -c ${SAMPLES}/${title}.cfg -I ${SAMPLES}/test.${nbod}.in.txt -O ${SAMPLES}/test.${nbod}.out.txt -v 100 nbod=${nbod}) 
+ENDMACRO(TEST_INTEGRATOR)
+
+
+
+############ ACTUAL TEST Cases Begin Here
 
 ADD_TEST(NAME basic COMMAND swarm integrate --defaults )
 
-ADD_TEST(NAME CPU COMMAND swarm test -c ${SAMPLES}/hermite_cpu.cfg -I ${SAMPLES}/smalltest.in.txt -O ${SAMPLES}/smalltest.out.txt )
 
-MACRO(TEST_INTEGRATOR title cfgname)
-	ADD_TEST(NAME Verify_${title}
-		COMMAND swarm test -c ${SAMPLES}/${cfgname} -I ${SAMPLES}/test.in.txt -O ${SAMPLES}/test.out.txt -v 100) 
-ENDMACRO(TEST_INTEGRATOR)
+TEST_INTEGRATOR(CPU 3)
 
-TEST_INTEGRATOR(Hermite_GPU hermite.cfg)
-TEST_INTEGRATOR(Hermite_adap_GPU hermite_adap.cfg)
-TEST_INTEGRATOR(Runge_Kutta_Fixed_Time_Step rkckf.cfg)
-TEST_INTEGRATOR(Runge_Kutta_Adaptive_Time_Step rkcka.cfg)
-TEST_INTEGRATOR(Euler euler.cfg)
-TEST_INTEGRATOR(Midpoint midpoint.cfg)
-TEST_INTEGRATOR(Verlet verlet.cfg)
+SET(nbod 3)
+WHILE(NOT nbod GREATER ${MAX_NBODIES})
+	TEST_INTEGRATOR(Hermite ${nbod})
+	TEST_INTEGRATOR(Hermite_Adaptive ${nbod})
+	TEST_INTEGRATOR(Runge_Kutta_Fixed_Time_Step ${nbod})
+	TEST_INTEGRATOR(Runge_Kutta_Adaptive_Time_Step ${nbod})
+	TEST_INTEGRATOR(Euler ${nbod})
+	TEST_INTEGRATOR(Midpoint ${nbod})
+	TEST_INTEGRATOR(Verlet ${nbod})
+	MATH( EXPR nbod "${nbod} + 1")
+ENDWHILE(NOT nbod GREATER ${MAX_NBODIES})
+
+
+## Add your test cases after this line (or include a file containing test cases)
+
+## Including a macro file (should be in cmake format)
+# INCLUDE(mytestcases.cmake)
+
+## Add a test case
+# ADD_TEST(NAME blah COMMAND swarm .... )
