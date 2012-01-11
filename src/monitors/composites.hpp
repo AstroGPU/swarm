53840f0f0d958e12af2739f88c475610320f5808
Saleh Dindar <salehqt@gmail.com>
2012-01-11 16:55:31 -0500

    New syntax for combining monitors and composites.hpp contains monitors composed of other ones.

--- src/monitors/composites.hpp
+++ src/monitors/composites.hpp
@@ -17,59 +17,44 @@
  ************************************************************************/
 #pragma once
 
-#include <limits>
+#include "combine.hpp"
+#include "stop_on_ejection.hpp"
+#include "stop_on_crossing_orbit.hpp"
+#include "stop_on_close_encounter.hpp"
 
-namespace swarm {
-namespace monitors {
+namespace swarm { namespace monitors {
 
-template< class Param1, class Param2 >
-struct combine_monitors_params {
-	Param1 p1;
-	Param2 p2;
-	combine_monitors_params(const config &cfg): p1(cfg), p2(cfg)
-	{
-	}
-};
 
-/** Template to allow developer to join two monitors
- *  Signal is true if either monitor returns true.
+/** Combination of stop_on_ejcetion, stop_on_close_encounter and
+ * stop_on_crossing_orbit
  *
- *  The order of the monitors is important. They are executed in order and
- *  in case of stoppers it can affect the execution. If the first one flags
- *  the system as disabled and the second one flags it as inactive then there is
- *  a problem.
- *
- *  It can be used in an expression to join more than two monitors
- *  Example:
- *  combine_monitors< combine_monitors< Monitor1, Monitor2> , Monitor3 >
- *  \ingroup monitors
  */
-template< class log_t,  class Monitor1,  class Monitor2 >
-struct combine {
-
-	typedef Monitor1 monitor1_t;
-	typedef Monitor2 monitor2_t;
-	public:
-	typedef combine_monitors_params<typename monitor1_t::params,typename monitor2_t::params > params;
-
-	params _params;
-
-	private:
-	monitor1_t _monitor1;
-	monitor2_t _monitor2;
-
-	public:
-
-	GPUAPI void operator () () { 
-	  _monitor1();
-	  _monitor2();
-	}
-
-	GPUAPI combine(const params& p,ensemble::SystemRef& s,log_t& l)
-		:_params(p),_monitor1(p.p1,s,l),_monitor2(p.p2,s,l){}
+template <class L> 
+struct stop_on_crossing_orbit_or_close_approach {
+	struct params {
+		typename stop_on_ejection<L>        ::params ej;
+		typename stop_on_close_encounter<L> ::params ce;
+		typename stop_on_crossing_orbit<L>  ::params co;
+		
+		params(const config& cfg)
+			:ej(cfg), ce(cfg), co(cfg) {}
+	};
 	
+	GPUAPI stop_on_crossing_orbit_or_close_approach
+		(const params& p,ensemble::SystemRef& s,L& l)
+		: ej(p.ej,s,l), ce(p.ce,s,l), co(p.co,s,l) 	{}
+	
+	GPUAPI void operator () () {
+		ej(); ce(); co();
+	}
+	
+private:
+	stop_on_ejection<L>        ej;
+	stop_on_close_encounter<L> ce;
+	stop_on_crossing_orbit<L>  co;
 };
 
-}
+} } // end namespace monitors :: swarm
 
-}
+
+
