26498619c59880fabbd3d0bdf0df6a902d046f74
Saleh Dindar <sale@cise.ufl.edu>
2012-01-31 20:32:49 -0500

    Blocksize test for all bodies

--- master_blocksize_test.sh
+++ master_blocksize_test.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-for CS in 1 2 4 8 16 32
+for CS in 4 8 16 32
 do
-	cmake . -DSHMEM_CHUNK_SIZE=$CS -DENSEMBLE_CHUNK_SIZE=$CS
+	cmake . -DSHMEM_CHUNK_SIZE=$CS -DENSEMBLE_CHUNK_SIZE=$CS > /dev/null
 	grep CHUNK_SIZE CMakeCache.txt
-	make clean
-	make swarm
-	bin/swarm -c blocktest.cfg benchmark --range system_per_block=${CS}..${CS}..64
+	make clean > /dev/null
+	make swarm > /dev/null
+	for n in 3 4 5 6 7 8 9
+	do
+		bin/swarm -c blocktest.cfg benchmark --range system_per_block=${CS}..${CS}..64 nbod=$n
+	done
+	echo "----------------------------------------------------------------------------------"
 done
