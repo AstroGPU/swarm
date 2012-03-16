880b79c97deb882fb80286c71ba05713ed646756
Saleh Dindar <salehqt@gmail.com>
2012-03-16 01:27:06 -0400

    sample python code to use swarm

---
+++ py/swarm.py
@@ -0,0 +1,37 @@
+from sys import path
+path.append('lib')
+
+from numpy import *
+from libswarmng_ext import *
+
+
+cfg = Config()
+
+cfg["integrator"] = "hermite_adap"
+cfg["time_step_factor"] = "170e-4"
+cfg["min_time_step"] = "1e-7"
+cfg["max_time_step"] = "1e-2"
+cfg["nsys" ] = "16"
+cfg["nbod"] = "3"
+cfg["time_step"] = "0.001"
+cfg["log_writer"] = "null"
+
+ref = generate_ensemble( cfg )
+ens = ref.clone()
+
+init(cfg)
+
+integ = Integrator.create( cfg )
+
+integ.ensemble = ens
+integ.destination_time = 1.0
+
+sync
+
+integ.integrate()
+
+sync
+
+
+max_deltaE = find_max_energy_conservation_error( ens, ref)
+print "Max energy conservation error ", max_deltaE
