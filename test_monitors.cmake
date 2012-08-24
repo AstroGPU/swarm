6ace16b88decee18bbcbc793022e0a4565cc40bf
Saleh Dindar <salehqt@gmail.com>
2012-08-23 22:25:16 -0400

    adding a checkbox to cmake. fixing regerate problem.

--- test_monitors.cmake
+++ test_monitors.cmake
@@ -1,5 +1,6 @@
 
-SET(PATH ${PATH} ${CMAKE_BINARY_DIR})
+SET(TEST_monitors_nbod TRUE CACHE BOOL "Test the monitors against different number of bodies")
+
 SET(TEST_MONITOR_DIR ${TESTDIR}/monitors)
 
 ADD_CUSTOM_TARGET(testcases)
@@ -11,7 +12,6 @@ MACRO(TEST_MONITOR title subject nbod)
 
 	# Create a make target for generating the test case data
 	ADD_CUSTOM_TARGET(Generate_Testcase_${title}_on_${subject}_with_${nbod}_Bodies
-		COMMAND ${DD}/generate ${nbod} ${DD}/${nbod}.in.txt
 		COMMAND swarm integrate -c ${DD}/${subject}.cfg -I ${DD}/${nbod}.in.txt -O ${DD}/${nbod}.out.txt -v 100 nbod=${nbod}) 
 	ADD_DEPENDENCIES(Generate_Testcase_${title}_on_${subject}_with_${nbod}_Bodies Generate_Testcase_${title}_with_${nbod}_Bodies)
 	ADD_DEPENDENCIES(testcases Generate_Testcase_${title}_on_${subject}_with_${nbod}_Bodies)
@@ -25,9 +25,9 @@ MACRO(ADD_MONITOR_TESTCASE title nbod)
 		COMMAND ${DD}/generate ${nbod} ${DD}/${nbod}.in.txt)
 ENDMACRO()
 
-ADD_MONITOR_TESTCASE(close_orbits 4)
-
-TEST_MONITOR(close_orbits HermiteEjection 4)
-
-TEST_MONITOR(close_orbits MVSEjection 4)
+IF(TEST_monitors_nbod)
+	ADD_MONITOR_TESTCASE(close_orbits 4)
+	TEST_MONITOR(close_orbits HermiteEjection 4)
+	TEST_MONITOR(close_orbits MVSEjection 4)
+ENDIF()
 
