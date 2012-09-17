208504782034a6045a569e3714eff8f712a75cc8
Saleh Dindar <salehqt@gmail.com>
2012-09-17 19:20:39 -0400

    adding scripts for benchmarking GPU

---
+++ scripts/saturation/by_integrators.sh
@@ -0,0 +1,27 @@
+DTIME=10.0 # Destination time
+NBOD="3 10" # a list of number of bodies
+INTEGRATORS="Hermite" # a list of integrators
+SOURCE=`dirname $0`/../../
+NSYS=`python $SOURCE/scripts/saturation/ranges.py` # auto generated list of nsys range
+
+# Guard against missing arguments
+if [ $# -lt 1 ]
+then
+	echo "Syntax: $0 <output directory>"
+	exit -1
+fi
+
+OUTPUTDIR="$1"
+mkdir -p $OUTPUTDIR
+
+for I in $INTEGRATORS
+do
+for N in $NBOD
+do
+	echo "Starting $N bodies, $I --------------------------------"
+	mkdir -p $OUTPUTDIR/$I
+	O=$OUTPUTDIR/$I/$N.csv
+	swarm benchmark -c $SOURCE/samples/$I.cfg --range nsys=$NSYS \
+		nsys=2000 nbod=$N -d $DTIME -v 100 2> $OUTPUTDIR/$I/$N-log.txt | tail -n +10 > $O 
+done
+done
