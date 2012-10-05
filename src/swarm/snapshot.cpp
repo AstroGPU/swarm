0e6749b55ffbcb60c423fa26903b6758b5c22dc0
Ying Zhang <zhang@hpc.ufl.edu>
2012-10-05 16:21:34 -0400

    Made a correction in file name.

--- src/swarm/snapshot.cpp
+++ src/swarm/snapshot.cpp
@@ -16,7 +16,7 @@
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ************************************************************************/
 
-/*! \file query.cpp
+/*! \file snapshot.cpp
  *   \brief Impletes load and save functions for ensemble files. 
  *
  *
