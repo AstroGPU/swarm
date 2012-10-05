b2b6af8130d28cbfcc2317f33f2f3fc02a94fdac
Ying Zhang <zhang@hpc.ufl.edu>
2012-10-05 16:14:53 -0400

    Added the brief description.

--- src/swarm/snapshot.cpp
+++ src/swarm/snapshot.cpp
@@ -16,6 +16,12 @@
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ************************************************************************/
 
+/*! \file query.cpp
+ *   \brief Impletes load and save functions for ensemble files. 
+ *
+ *
+*/
+
 #include "snapshot.hpp"
 
 namespace swarm {
