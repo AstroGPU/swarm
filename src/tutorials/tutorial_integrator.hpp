e07c5ffe2ed6f599f94b3c8d6c5661942265b3f7
Ying Zhang <zhang@hpc.ufl.edu>
2012-10-25 12:39:58 -0400

    Added file description section.

--- src/tutorials/tutorial_integrator.hpp
+++ src/tutorials/tutorial_integrator.hpp
@@ -15,6 +15,13 @@
  * Free Software Foundation, Inc.,                                       *
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ************************************************************************/
+
+/** \file tutorial_integrator.hpp
+ *  \brief Defines \ref TutorialIntegrator class - 
+ *         implements a tutorial for making a integrator.
+ *
+ */
+
 // @page TutorialIntegrator Tutorial for implementing an Integrator
 // If you are planning to implement a new ODE integration algorithm for
 // use with Swarm and a propagator is not enough for your needs, you
