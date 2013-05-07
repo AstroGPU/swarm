c0f29ce12dad2c347778da4ae3601034dc9f3e12
Saleh Dindar <saleh@cise.ufl.edu>
2013-05-07 05:32:34 -0400

    Cleaning up the pythong code

--- py/tutorial.py
+++ py/tutorial.py
@@ -3,53 +3,57 @@
 
 
 # @page TutorialPython Beginner Python Tutorial
-import sys
+# First thing is to import swarmng. It is located in <SOURCE DIRECTORY>/py/swarmng.py.
+# It takes care of loading the libswarmng_ext shared object. Please run the scripts
+# from your build directory so swarmng.py can find your libraries.
+import swarmng
 import os
 
-BASEDIR = os.path.dirname(os.path.realpath(__file__))
-sys.path.append('lib')
+# swarm functions cannot take hashes for config. So 
+# we have to create the special config object (a C++ std::map)
+# using the mkConfig function
+# This first line initializes Swarm-NG. 'nogpu' option
+# allows us to run this script on a machine without GPU
+swarmng.init(swarmng.mkConfig({ 'nogpu' : 1 }))
 
-# raoecuh
-#a.,rcuhc,r.uh
-from numpy import *
-from libswarmng_ext import *
-# ar,c.uh.,cu
-# ourcha
+# Now we can load an ensemble from a text file. The ensemble file is in the swarm source directory:
+ensemble_filename = os.path.join(swarmng.SWARMDIR , 'test/integrators/test.3.in.txt')
+# We use a simple call that loads the file and returns the data structure
+ref = swarmng.DefaultEnsemble.load_from_text( ensemble_filename )
 
-cfg = Config()
-
-cfg["integrator"] = "hermite_cpu"
-cfg["time_step_factor"] = "170e-4"
-cfg["min_time_step"] = "1e-7"
-cfg["max_time_step"] = "1e-2"
-cfg["nsys" ] = "16"
-cfg["nbod"] = "3"
-cfg["time_step"] = "0.001"
-cfg["log_writer"] = "null"
-cfg["nogpu"] = "1"
-
-
-def integrate(ens) :
-	init(cfg)
-	integ = Integrator.create( cfg )
-	integ.ensemble = ens
-	integ.destination_time = 1.0
-	sync
-	integ.integrate()
-	sync
-
-
-ref = DefaultEnsemble.load_from_text( os.path.join(BASEDIR , "../test/integrators/test.3.in.txt"))
-
+# We can also look into the data structure and view the values
 for s in range(ref.nsys):
-	for b in range(ref.nbod):
-		print("pos: %s\tvel:%s" % (ref[s][b].pos, ref[s][b].vel))
+  for b in range(ref.nbod):
+    print("pos: %s\tvel:%s" % (ref[s][b].pos, ref[s][b].vel))
 
+# We can also modify the values
 ref[0][0].pos = [ 1.0, 2.0 , 5.0 ]
 
+# We can easily make a copy of the whole data structure in this example
+# we need to have two ensembles: reference (ref) and a working copy (ens)
 ens = ref.clone()
-integrate(ens)
 
-max_deltaE = find_max_energy_conservation_error( ens, ref)
+# Now we have to select our integrator, swarmng.Integrator.create function
+# instantiates an integrator for us.
+# In this case, we use a CPU based implementation of Hermite PEC2 integrator(hermite_cpu)
+# To see what integrators are available, 'run bin/swarm -p' from command line.
+integrator_cfg = {
+  'integrator'       : 'hermite_cpu',
+  'time_step'        :  0.001       ,
+  }
+integ = swarmng.Integrator.create(swarmng.mkConfig(integrator_cfg))
+
+# we set up the parameters to the integrator
+integ.ensemble = ens
+integ.destination_time = 1.0
+
+# Now do the integration
+integ.integrate()
+
+
+# A very common task is to check the energy conservation 
+# the function swarmng.find_max_energy_conservation_error does this
+# task for us, given two ensembles.
+max_deltaE = swarmng.find_max_energy_conservation_error( ens, ref)
 print("Max energy conservation error %g" % max_deltaE)
 
