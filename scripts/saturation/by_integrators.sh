67ac2e3b35e05072cda76539b13477349b3084c3
Saleh Dindar <salehqt@gmail.com>
2013-08-26 11:43:34 -0400

    Updating saturation script for better output

--- scripts/saturation/by_integrators.sh
+++ scripts/saturation/by_integrators.sh
@@ -1,5 +1,5 @@
 DTIME=2.0 # Destination time
-NBOD="3 10" # a list of number of bodies
+NBOD="3 4 6 8 10" # a list of number of bodies
 INTEGRATORS="Hermite" # a list of integrators
 SOURCE=`dirname $0`/../../
 NSYS=`python $SOURCE/scripts/saturation/ranges.py` # auto generated list of nsys range
@@ -9,8 +9,8 @@ NSYS=`python $SOURCE/scripts/saturation/ranges.py` # auto generated list of nsys
 # Guard against missing arguments
 if [ $# -lt 1 ]
 then
-	echo "Syntax: $0 <output directory>"
-	exit -1
+    echo "Syntax: $0 <output directory>"
+    exit -1
 fi
 
 OUTPUTDIR="$1"
@@ -20,10 +20,10 @@ for I in $INTEGRATORS
 do
 for N in $NBOD
 do
-	echo "Starting $N bodies, $I --------------------------------"
-	mkdir -p $OUTPUTDIR/$I
-	O=$OUTPUTDIR/$I/$N.csv
-	$BUILD/bin/swarm benchmark -c $SOURCE/samples/$I.cfg --range nsys=$NSYS verbose=1 \
-		nsys=2000 nbod=$N -d $DTIME -v 100 2> $OUTPUTDIR/$I/$N-log.txt | tail -n +10 > $O 
+    echo "Starting $N bodies, $I --------------------------------"
+    mkdir -p $OUTPUTDIR/$I
+    O=$OUTPUTDIR/$I/$N.csv
+    $BUILD/bin/swarm benchmark -c $SOURCE/samples/$I.cfg --range nsys=$NSYS verbose=1 \
+        nsys=2000 nbod=$N -d $DTIME verbose=1 -v 100 2> $OUTPUTDIR/$I/$N-log.txt | tail -n +10 > $O 
 done
 done
